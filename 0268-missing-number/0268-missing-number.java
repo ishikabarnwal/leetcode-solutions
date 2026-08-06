3880e5d9562c0853ff75be2a2b982100ae0c9cb2
Ishika Barnwal <167006718+ishikabarnwal@users.noreply.github.com>
2026-08-06 15:35:56 +0530

    Time: 0 ms (100%), Space: 47.4 MB (33.21%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,33 +1,33 @@
-// class Solution {
-//     public int missingNumber(int[] nums) {
-//         int n = nums.length;
-//         int xor = 0;
-
-//         // XOR all indices and array values
-//         for (int i = 0; i < n; i++) {
-//             xor ^= i;
-//             xor ^= nums[i];
-//         }
-
-//         // XOR with n (last number in range)
-//         xor ^= n;
-
-//         return xor;
-//     }
-// }
 class Solution {
     public int missingNumber(int[] nums) {
-
         int n = nums.length;
+        int xor = 0;
 
-        int expectedSum = n * (n + 1) / 2;
-
-        int actualSum = 0;
-
-        for (int num : nums) {
-            actualSum += num;
+        // XOR all indices and array values
+        for (int i = 0; i < n; i++) {
+            xor ^= i;
+            xor ^= nums[i];
         }
 
-        return expectedSum - actualSum;
+        // XOR with n (last number in range)
+        xor ^= n;
+
+        return xor;
     }
 }
+// class Solution {
+//     public int missingNumber(int[] nums) {
+
+//         int n = nums.length;
+
+//         int expectedSum = n * (n + 1) / 2;
+
+//         int actualSum = 0;
+
+//         for (int num : nums) {
+//             actualSum += num;
+//         }
+
+//         return expectedSum - actualSum;
+//     }
+// }
