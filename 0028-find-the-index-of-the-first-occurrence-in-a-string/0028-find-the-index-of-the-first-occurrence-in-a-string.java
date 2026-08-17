04c55648472bd3700308d6373ad9c23459897c12
Ishika Barnwal <167006718+ishikabarnwal@users.noreply.github.com>
2026-08-17 16:27:05 +0530

    Time: 2 ms (13.37%), Space: 43.1 MB (33.81%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,29 +1,22 @@
-// class Solution {
-//     public int strStr(String haystack, String needle) {
-//         for(int i=0;i<=haystack.length()-needle.length();i++){
-//             int j;
-//             for(j=0;j<needle.length();j++){
-//                 if(haystack.charAt(i+j)!=needle.charAt(j)){
-//                     break;
-//                 }
-//             }
-//             if(j==needle.length()){
-//                 return i;
-//             }
-//         }
-//         return -1;
-//     }
-// }
+class Solution {
+    public int strStr(String haystack, String needle) {
+        for(int i=0;i<=haystack.length()-needle.length();i++){
+            int j;
+            for(j=0;j<needle.length();j++){
+                if(haystack.charAt(i+j)!=needle.charAt(j)){
+                    break;
+                }
+            }
+            if(j==needle.length()){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
+//not recommended but yeah it works as well
 // class Solution {
 //     public int strStr(String haystack, String needle) {
 //         return haystack.indexOf(needle);
 //     }
 // }
-class Solution {
-    public int strStr(String haystack, String needle) {
-        if(haystack.contains(needle))
-        return haystack.indexOf(needle);
-
-        return -1;
-    }
-}
