b90b7529dd429086916e8961a7ad80d8c786f29d
Ishika Barnwal <167006718+ishikabarnwal@users.noreply.github.com>
2026-08-17 16:25:48 +0530

    Time: 0 ms (100%), Space: 43.3 MB (18.63%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -14,8 +14,16 @@
 //         return -1;
 //     }
 // }
+// class Solution {
+//     public int strStr(String haystack, String needle) {
+//         return haystack.indexOf(needle);
+//     }
+// }
 class Solution {
     public int strStr(String haystack, String needle) {
+        if(haystack.contains(needle))
         return haystack.indexOf(needle);
+
+        return -1;
     }
 }
